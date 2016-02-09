d169083b2051a7b16972071a00c7f2ddf3d68452
fernandoWPF <fernando.wesleypf@gmail.com>
2016-02-09 18:21:49 -0200

    Segundo Commit

---
+++ src/main/java/br/com/consultorio_odontologico/bean/ProcedimentoBean.java
@@ -0,0 +1,50 @@
+package br.com.consultorio_odontologico.bean;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class ProcedimentoBean implements Serializable, GenericBean {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void novo() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void salvar() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void excluir() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void carregarPesquisa() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void carregarCadastro() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void editar() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
