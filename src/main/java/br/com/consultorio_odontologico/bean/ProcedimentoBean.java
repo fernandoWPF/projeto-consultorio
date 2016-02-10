8e9c5ce3ec61bb4e34efc3b8ef520b92f88683b9
fernandoWPF <fernando.wesleypf@gmail.com>
2016-02-09 23:50:02 -0200

    Adicionado o ProcedimentoBean

--- src/main/java/br/com/consultorio_odontologico/bean/ProcedimentoBean.java
+++ src/main/java/br/com/consultorio_odontologico/bean/ProcedimentoBean.java
@@ -1,9 +1,14 @@
 package br.com.consultorio_odontologico.bean;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+
+import br.com.consultorio_odontologico.dao.ProcedimentoDAO;
+import br.com.consultorio_odontologico.domain.Procedimento;
+import br.com.consultorio_odontologico.util.FacesUtil;
 
 @ManagedBean
 @ViewScoped
@@ -11,40 +16,116 @@ public class ProcedimentoBean implements Serializable, GenericBean {
 
 	private static final long serialVersionUID = 1L;
 
+	List<Procedimento> procedimentos;
+	Procedimento procedimentoCadastro;
+
+	private String acao;
+	private Long id;
+
+	public List<Procedimento> getProcedimentos() {
+		return procedimentos;
+	}
+
+	public void setProcedimentos(List<Procedimento> procedimentos) {
+		this.procedimentos = procedimentos;
+	}
+
+	public Procedimento getProcedimentoCadastro() {
+		return procedimentoCadastro;
+	}
+
+	public void setProcedimentoCadastro(Procedimento procedimentoCadastro) {
+		this.procedimentoCadastro = procedimentoCadastro;
+	}
+
+	public String getAcao() {
+		return acao;
+	}
+
+	public void setAcao(String acao) {
+		this.acao = acao;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 	@Override
 	public void novo() {
-		// TODO Auto-generated method stub
+		procedimentoCadastro = new Procedimento();
 
 	}
 
 	@Override
 	public void salvar() {
-		// TODO Auto-generated method stub
+
+		try {
+			ProcedimentoDAO dao = new ProcedimentoDAO();
+			dao.salvar(procedimentoCadastro);
+			procedimentoCadastro = new Procedimento();
+			FacesUtil.addMsgInfo("Procedimento Salvo com Sucesso!");
+		} catch (Exception e) {
+			FacesUtil.addMsgError("Houve um erro ao Salvar o Procedimento!\n" + e.getMessage());
+		}
 
 	}
 
 	@Override
 	public void excluir() {
-		// TODO Auto-generated method stub
+
+		try {
+			ProcedimentoDAO dao = new ProcedimentoDAO();
+			dao.excluir(procedimentoCadastro);
+
+			FacesUtil.addMsgInfo("Procedimento Excluído com Sucesso!");
+		} catch (Exception e) {
+			FacesUtil.addMsgError("Houve um erro ao Excluir o Procedimento!\n" + e.getMessage());
+		}
 
 	}
 
 	@Override
 	public void carregarPesquisa() {
-		// TODO Auto-generated method stub
+
+		try {
+			ProcedimentoDAO dao = new ProcedimentoDAO();
+			procedimentos = dao.findAll();
+
+		} catch (Exception e) {
+			FacesUtil.addMsgError("Houve um erro ao Listar os Procedimentos!\n" + e.getMessage());
+		}
 
 	}
 
 	@Override
 	public void carregarCadastro() {
-		// TODO Auto-generated method stub
+
+		try {
+			if (id != null) {
+				ProcedimentoDAO dao = new ProcedimentoDAO();
+				procedimentoCadastro = dao.getById(id);
+			} else {
+				procedimentoCadastro = new Procedimento();
+			}
+		} catch (Exception e) {
+			FacesUtil.addMsgError("Houve um erro ao Carregar o Procedimento!\n" + e.getMessage());
+		}
 
 	}
 
 	@Override
 	public void editar() {
-		// TODO Auto-generated method stub
-
+		try {
+			ProcedimentoDAO dao = new ProcedimentoDAO();
+			dao.update(procedimentoCadastro);
+			FacesUtil.addMsgInfo("Dados Atualizados com Sucesso!");
+		} catch (Exception e) {
+			FacesUtil.addMsgError("Houve um erro ao Alterar o Procedimento\n: " + e.getMessage());
+		}
 	}
 
 }
